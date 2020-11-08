25c51ff611db4fbeea4e6a69da612f1069d6c61e
MariaPSegarra <mariapsegarra@gmail.com>
2020-11-08 20:49:43 +0000

    Create an Item. Manage Forms with Django

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
+# display todo items
 def get_todo_list(request):
     items = Item.objects.all()
     # context is dict with all our items
@@ -11,3 +13,17 @@ def get_todo_list(request):
     }
     return render(request, 'todo/todo_list.html', context)
     # context as a 3rd argument to render function
+
+
+# add new item
+def add_item(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+           form.save()
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
